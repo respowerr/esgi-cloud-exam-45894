336481b5dd6ff37e95584a65e120538d2dcf4e1c
respowerr <jules.nr@orange.fr>
2024-04-02 12:08:34 +0200

    add postgres creds with var_env

--- db.client.js
+++ db.client.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize')
 
 // database
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_PASS = process.env.DB_PASS;
+const DB_USER = process.env.DB_USER;
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
   {
+    host: DB_HOST,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
